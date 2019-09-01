e25f5670f1615273a9a165bb401aec491fc338f6
clarewu1106 <54608737+clarewu1106@users.noreply.github.com>
2019-09-01 17:06:51 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+Name: Dongying Wu
+Student ID: 729678
+Due day: 09.01.19
+Class: MSITM 6341
+
+print ("Hello World!")
+print ("My name is: Dongying Wu")
+print ("This is my first Python Program!")
+print ("Have a nice day.")
