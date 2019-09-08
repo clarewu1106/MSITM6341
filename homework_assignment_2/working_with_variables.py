48540cadf6de6b07d8bf7ba44aae1aada73b632c
clarewu1106 <54608737+clarewu1106@users.noreply.github.com>
2019-09-08 18:39:43 -0500

    Add files via upload

---
+++ homework_assignment_2/working_with_variables.py
@@ -0,0 +1,27 @@
+# Name: Dongying Wu
+# Student ID: 729678
+# Due date: September 8, 2019
+# Class: #MSITM 6341
+
+
+# Variable of Stock Symbol
+stock_symbol = "appl"
+# Variable of Price
+price = 204.66
+# Variable of Change
+change = 4.08
+# Dollar Amount Calculation formula 
+dollar_amount_changed = price - change
+
+# Print Dollar Sign with Price
+dollar_sign = '${}'.format(price)
+# Limit Dollar Amount Changed with Two Decimal Numbers
+two_decimal = "%.2f" % dollar_amount_changed
+
+
+# Output_1
+print ("Symbol:" + stock_symbol.upper() + ", " + "Price:" + str(price) + ", " + "Change:" + str(-change)) 
+# Output_2
+print ("Symbol:" + stock_symbol.lower() + ", " + "Price:" + str(dollar_sign) + ", " + "Change:" + str(-change))
+# Output_3
+print ("Symbol:" + stock_symbol.upper() + " --- " + "Yesterday's Price:" + str (two_decimal)) 
